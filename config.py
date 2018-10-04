643aa8a78306fb0355747925a1a343e2b1c3b1c8
harsh2013 <harsh2013@gmail.com>
2018-10-04 20:54:25 +0530

    Initial Commit: First Starting to deployment.

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['harsh2013@gmail.com']
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    POSTS_PER_PAGE = 25
+
+    #UNCOMMENT THE CODE WRITTEN BELOW TO SEND MAIL
+    #OR CHECH ON NET WHY THIS ERROR IS COMING
+    # raise SMTPServerDisconnected('please run connect() first')
+
+
+
+    # MAIL_SERVER = 'smtp.gmail.com'
+    # MAIL_PORT = 465
+    # MAIL_USE_TLS = False
+    # MAIL_USE_SSL = True
+    # MAIL_USERNAME = 'harsh2013@gmail.com'
+    # MAIL_PASSWORD = 'sudhansu2013'
+    # ADMINS = ['harsh2013@gmail.com']
+    # POSTS_PER_PAGE = 2
